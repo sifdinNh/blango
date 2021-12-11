3d3fc71408e78c60019bcef987e38382d89603cb
Sifdin Nahhas <sifdinnh@gmail.com>
2021-12-11 12:49:03 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 
 ]
